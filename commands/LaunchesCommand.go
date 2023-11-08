cc127b94dce89f75a7ae58ad6ce925f92aa04e26
Yuriy Sirotenko <ysirotenko@jmart.kz>
2023-11-08 16:11:27 +0600

    err and command result as return

--- commands/LaunchesCommand.go
+++ commands/LaunchesCommand.go
@@ -14,34 +14,42 @@ const (
 	FormatReset = "\033[0m"
 )
 
-func LaunchesCommand(page *int) {
+func LaunchesCommand(page *int) (string, error) {
+	var result string = ""
+
 	command := apis.GetLaunchUpcomingRequest()
 	command.Page = *page * 10
 	params := "?offset=" + strconv.Itoa(command.Page)
- 	response, _ := http.Get(command.Domain + command.Ver + command.Url + params)
+ 	response, errHttp := http.Get(command.Domain + command.Ver + command.Url + params)
  	defer response.Body.Close()
+ 	
+ 	if errHttp != nil {
+ 		return result, errHttp
+ 	}
  	// todo err
  	// todo status code
- 	b, err := ioutil.ReadAll(response.Body)
+ 	b, errReader := ioutil.ReadAll(response.Body)
  	
- 	if err != nil {
- 		panic("err 0")
+ 	if errReader != nil {
+ 		return result, errReader
  	}
  	
  	var structmy apis.LaunchUpcomingResponse
- 	err = json.Unmarshal(b, &structmy)
+ 	errJson := json.Unmarshal(b, &structmy)
  	
- 	if err != nil {
- 		panic("err")
+ 	if errJson != nil {
+ 		return result, errJson
  	}
  	// todo err
  	
  	for _, entity := range structmy.Results {
- 		fmt.Printf("%vID:%v %v\n", FormatBold, FormatReset, entity.Id)
- 		fmt.Printf("%vName:%v %v\n", FormatBold, FormatReset, entity.Name)
- 		fmt.Printf("%vDate:%v %v\n", FormatBold, FormatReset, entity.Date)
- 		fmt.Printf("%vStatus:%v %v\n", FormatBold, FormatReset, entity.Status.Name)
- 		fmt.Printf("%vDescription:%v %v\n", FormatBold, FormatReset, entity.Status.Description)
- 		fmt.Printf("*------------------*\n\n")
+ 		result += fmt.Sprintf("%vID:%v %v\n", FormatBold, FormatReset, entity.Id)
+ 		result += fmt.Sprintf("%vName:%v %v\n", FormatBold, FormatReset, entity.Name)
+ 		result += fmt.Sprintf("%vDate:%v %v\n", FormatBold, FormatReset, entity.Date)
+ 		result += fmt.Sprintf("%vStatus:%v %v\n", FormatBold, FormatReset, entity.Status.Name)
+ 		result += fmt.Sprintf("%vDescription:%v %v\n", FormatBold, FormatReset, entity.Status.Description)
+ 		result += fmt.Sprintf("*------------------*\n\n")
  	}
+ 	
+ 	return result, nil
 }
--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"flag"
 	"ysirotenko.com/thespacedevs-cmd/commands"
+	"fmt"
 )
 
 func main() {
 	action := flag.String("action", "", "Action list")
 	page := flag.Int("page", 1, "Page number")
 	flag.Parse()
+	
+	var err error
+	var result string
 
 	switch *action {
 		case "launches":
-			commands.LaunchesCommand(page)
+			result, err = commands.LaunchesCommand(page)
 	}
+	
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println(result)
 }
